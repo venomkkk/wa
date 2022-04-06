b8cd0a21077e087981e0d018746b694a5c52b45f
V <78717939+venomkkk@users.noreply.github.com>
2022-04-06 01:48:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ PURPULA='\033[35m'
 
 #Atualizar...
 print(f'{GREEN}Atualizando o script...')
-os.systemtqdm(('rm -rf * && git clone https://github.com/venomkkk/wa && cd wa && cp * .. && cd .. && rm -rf wa'))
+os.system(tqdm('rm -rf * && git clone https://github.com/venomkkk/wa && cd wa && cp * .. && cd .. && rm -rf wa'))
 ##################################
  
 def init():
