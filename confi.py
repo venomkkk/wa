703e899897b92cb5450a3af1906ff14e9719add1
V <78717939+venomkkk@users.noreply.github.com>
2022-04-07 11:19:57 -0300

    Update confi.py

--- confi.py
+++ confi.py
@@ -3,6 +3,24 @@ import os
 
 # Biblioteca
 
+#Cores
+BRANCO='\033[1;29m'
+VermelhoAll='\e[01;37;41m'
+NADA='\033[0m'
+CINZA='\033[02;37m'
+DESTACAR='\033[01;37m'
+RED='\033[1;31m'
+AZUL='\033[36m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+AMARELO='\033[33m'
+SCOLOR='\033[0m'
+SEMCOR='\033[8m'
+BrancoAll='\033[03;37;47m'
+LETRAPRETA='\033[02;30;47m'
+PURPULA='\033[35m'
+###############################
+
 #Emails
 emailsuport = "support@support.whatsapp.com"
 email1 = "emailprobot001@gmail.com"
@@ -22,4 +40,4 @@ banner = "Unban WA"
 
 
 def manutencao():
-  print(f' Estamos em manutenção...\n Volte novamente mais tarde ;)')
+  print(f'{RED} Estamos em manutenção...\n Volte novamente mais tarde ;)')
